8b2f9452d5e9d85931a759d0f7707146dda563c6
Mackenzzy <mackenzzy@gmail.com>
2023-11-02 14:33:59 -0300

    botao voltar

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,9 +2,11 @@ import styles from './NotFound.module.scss';
 import { ReactComponent as NotFoundImage} from 'assets/not_found.svg'
 import classNames from 'classnames';
 import stylesTema from 'styles/Tema.module.scss';
+import { useNavigate } from 'react-router-dom'
 
 
 export default function NotFound() {
+    const navigate = useNavigate();
     return (
         <div className={classNames({
             [styles.container]: true,
@@ -12,8 +14,23 @@ export default function NotFound() {
 
 
         })}>
+            <div className={styles.voltar}>
+                <button onClick ={()=> navigate(-1)}>
+                    {'< Voltar'}
+                </button>
+            </div>
             <NotFoundImage/>
         </div>
     )
     
 }
+
+
+
+
+/*          <div className={styles.voltar}>
+                <button onClick ={()=> window.history.back()}>
+                    {'< Voltar'}
+                </button>
+            </div>
+*/
